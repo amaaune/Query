96ee88448a92dd5535766b1568a244cb6cf659f2
aamaury <amaury.aune@ynov.com>
2026-05-05 09:21:00 +0200

    [ADD] Premiere base

---
+++ js/qcm.js
@@ -0,0 +1,10 @@
+const question = {
+    question: "",
+    reponses: ["A. ", "B. ", "C. ", "D. "],
+    valid: 1
+};
+ const death;
+
+function qcm() {
+    alert("Hello! I am an alert box!!");
+};
