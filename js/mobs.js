c1a5e0a410e5b41f287604de448bcbca4199b2d3
aamaury <amaury.aune@ynov.com>
2026-05-16 21:32:29 +0200

    [FIX] Versioning nom ennemies + lien img

--- js/mobs.js
+++ js/mobs.js
@@ -1,6 +1,6 @@
 const bosses = [
-    {name: "Zombie", pv: 20, level: 0},
-    {name: "Student", pv: 25, level: 1},
-    {name: "Yamao", pv: 30, level: 2},
-    {name: "Remy", pv:40, level: 3}
+    {name: "Junior", pv: 20, level: 0, img: "../img/mobs/junior.jpg"},
+    {name: "Senior", pv: 25, level: 1, img: "../img/mobs/senior.jpg"},
+    {name: "Cto", pv: 30, level: 2, img: "../img/mobs/cto.jpg"},
+    {name: "Founder", pv: 40, level: 3, img: "../img/mobs/founder.jpg"}
 ];
