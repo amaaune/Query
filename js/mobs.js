10f1728e3e5ce75096936f723c66a9fe6435edd8
aamaury <amaury.aune@ynov.com>
2026-05-19 11:40:49 +0200

    [FIX] Fin des test -> pv mobs UP

--- js/mobs.js
+++ js/mobs.js
@@ -1,6 +1,6 @@
 const bosses = [
-    {name: "Junior", pv: 20, level: 0, img: "../img/mobs/junior.jpg"},
-    {name: "Senior", pv: 25, level: 1, img: "../img/mobs/senior.jpg"},
-    {name: "Cto", pv: 30, level: 2, img: "../img/mobs/cto.jpg"},
-    {name: "Founder", pv: 40, level: 3, img: "../img/mobs/founder.jpg"}
+    {name: "Junior", pv: 60, level: 0, img: "../img/mobs/junior.jpg"},
+    {name: "Senior", pv: 90, level: 1, img: "../img/mobs/senior.jpg"},
+    {name: "Cto", pv: 120, level: 2, img: "../img/mobs/cto.jpg"},
+    {name: "Founder", pv: 180, level: 3, img: "../img/mobs/founder.jpg"}
 ];
